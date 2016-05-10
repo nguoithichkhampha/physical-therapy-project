fb49b787614f844c321c8ce1112aaa9f13fadb92
Cuong Trinh <cuong@emotiv.com>
2016-05-10 14:45:50 +0700

    Add source code

---
+++ Alpha project/Assets/LeapMotion/Scripts/Emotiv/EmoEngineController.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using Emotiv;
+
+public class EmoEngineController
+{
+	EmoEngine engine = EmoEngine.Instance; 
+	EdkDll.IEE_MentalCommandAction_t[] list_action =
+		{
+		EdkDll.IEE_MentalCommandAction_t.MC_NEUTRAL,
+		EdkDll.IEE_MentalCommandAction_t.MC_PUSH
+		};
+	public EmoEngineController()
+	{
+		EmoEngine engine = EmoEngine.Instance;
+
+		engine.EmoEngineConnected +=
+			new EmoEngine.EmoEngineConnectedEventHandler(engine_EmoEngineConnected);
+		engine.EmoEngineDisconnected +=
+			new EmoEngine.EmoEngineDisconnectedEventHandler(engine_EmoEngineDisconnected);
+		engine.UserAdded +=
+			new EmoEngine.UserAddedEventHandler(engine_UserAdded);
+		engine.UserRemoved +=
+			new EmoEngine.UserRemovedEventHandler(engine_UserRemoved);
+		engine.EmoStateUpdated +=
+			new EmoEngine.EmoStateUpdatedEventHandler(engine_EmoStateUpdated);
+
+		/*For mental command detection*/
+		engine.MentalCommandTrainingCompleted += 
+			new EmoEngine.MentalCommandTrainingCompletedEventHandler (engine_MentalCommandTrainingCompleted);
+		engine.MentalCommandTrainingRejected +=
+			new EmoEngine.MentalCommandTrainingRejectedEventHandler (engine_MentalCommandTrainingRejected);
+		engine.MentalCommandTrainingReset +=
+			new EmoEngine.MentalCommandTrainingResetEventHandler (engine_MentalCommandTrainingReset);
+		engine.MentalCommandTrainingStarted +=
+			new EmoEngine.MentalCommandTrainingStartedEventEventHandler (engine_MentalCommandTrainingStarted);
+		engine.MentalCommandTrainingSucceeded +=
+			new EmoEngine.MentalCommandTrainingSucceededEventHandler (engine_MentalCommandTrainingSucceeded);
+		engine.MentalCommandTrainingFailed +=
+			new EmoEngine.MentalCommandTrainingFailedEventHandler (engine_MentalCommandTrainingFailed);
+		engine.MentalCommandTrainingDataErased +=
+			new EmoEngine.MentalCommandTrainingDataErasedEventHandler (engine_MentalCommandTrainingErased);
+		engine.MentalCommandEmoStateUpdated +=
+			new EmoEngine.MentalCommandEmoStateUpdatedEventHandler (engine_MentalCommandEmoStateUpdated);
+		
+	}
+
+	static void engine_EmoEngineConnected(object sender, EmoEngineEventArgs e)
+	{
+		Debug.LogWarning ("Engine Connected");
+	}
+
+	static void engine_EmoEngineDisconnected(object sender, EmoEngineEventArgs e)
+	{
+		
+	}
+	static void engine_UserAdded(object sender, EmoEngineEventArgs e)
+	{
+		Debug.LogWarning ("User Added");
+	}
+	static void engine_UserRemoved(object sender, EmoEngineEventArgs e)
+	{
+		Debug.LogWarning ("User Removed");
+	}
+
+	static void engine_EmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+	{
+		EmoState es = e.emoState;
+
+	}  
+
+	static void engine_MentalCommandTrainingStarted(object sender, EmoEngineEventArgs e)
+	{
+		
+	}
+
+	static void engine_MentalCommandTrainingSucceeded(object sender, EmoEngineEventArgs e)
+	{
+		
+	}
+
+	static void engine_MentalCommandTrainingFailed(object sender, EmoEngineEventArgs e)
+	{
+
+	}
+
+	static void engine_MentalCommandTrainingCompleted(object sender, EmoEngineEventArgs e)
+	{
+		
+	}
+
+	static void engine_MentalCommandTrainingRejected(object sender, EmoEngineEventArgs e)
+	{
+		
+	}
+
+	static void engine_MentalCommandTrainingErased(object sender, EmoEngineEventArgs e)
+	{
+
+	}
+
+	static void engine_MentalCommandTrainingReset(object sender, EmoEngineEventArgs e)
+	{
+
+	}
+
+	static void engine_MentalCommandEmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+	{
+		
+	}
+
+	public void ConnectEngine()
+	{
+	    engine.Connect ();
+	}
+
+	public void ProcessEvent()
+	{
+		engine.ProcessEvents (1000);
+	}
+
+	public void TrainingAction(int index_action)
+	{
+		if (list_action[index_action] != EdkDll.IEE_MentalCommandAction_t.MC_NEUTRAL)
+			EdkDll.IEE_MentalCommandSetActiveActions (0, (uint)list_action[index_action]);
+		EdkDll.IEE_MentalCommandSetTrainingAction (0, list_action[index_action]);
+		EdkDll.IEE_MentalCommandSetTrainingControl (0, EdkDll.IEE_MentalCommandTrainingControl_t.MC_START);
+	}
+		
+}
+
---
+++ Alpha project/Assets/LeapMotion/Scripts/Utils/EmotivHandController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EmotivHandController : MonoBehaviour
+{
+
+	public HandModel leftGraphicsModel;
+	/** The GameObject containing colliders to use for the left hand or both hands if separateLeftRight is false. */
+	//public HandModel leftPhysicsModel;
+	/** The graphics hand model to use for the right hand. */
+	public HandModel rightGraphicsModel;
+	/** The physics hand model to use for the right hand. */
+	//public HandModel rightPhysicsModel;
+	// If this is null hands will have no parent
+	public Transform handParent = null;
+
+	public bool mirrorZAxis = false;
+
+	protected Dictionary<int, HandModel> hand_graphics_;
+
+	protected HandModel CreateHand(HandModel model) {
+		HandModel hand_model = Instantiate(model, transform.position, transform.rotation)
+			as HandModel;
+		hand_model.gameObject.SetActive(true);
+		if (handParent != null) {
+			hand_model.transform.SetParent(handParent.transform);
+		}
+		return hand_model;
+	}
+	// Use this for initialization
+	void Start ()
+	{
+		hand_graphics_ = new Dictionary<int, HandModel>();
+		HandModel new_hand = CreateHand(leftGraphicsModel);
+		new_hand.MirrorZAxis(mirrorZAxis);
+
+		float hand_scale = 3;
+		new_hand.transform.localScale = hand_scale * transform.lossyScale;
+
+		new_hand.InitHand();
+		hand_graphics_ [0] = new_hand;
+		//new_hand.SetController(this);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public HandModel[] GetAllGraphicsHands() {
+		if (hand_graphics_ == null)
+			return new HandModel[0];
+
+		HandModel[] models = new HandModel[hand_graphics_.Count];
+		hand_graphics_.Values.CopyTo(models, 0);
+		return models;
+	}
+}
+
